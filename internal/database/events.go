c58ed26a897abc1f91d17d57ac9488edf70262ea
贾丽鹏 <1094425279@qq.com>
2025-12-11 17:15:05 +0800

    commit

--- internal/database/events.go
+++ internal/database/events.go
@@ -1,6 +1,10 @@
 package database
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+	"time"
+)
 
 type EventModel struct {
 	DB *sql.DB
@@ -13,4 +17,13 @@ type Event struct {
 	Description string `json:"description" binding:"required,min=10"`
 	Date        string `json:"date" binding:"required,datetime=2006-01-02"`
 	Location    string `json:"location" binding:"required,min=3"`
+}
+
+func (m *EventModel) insert(event *Event) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := "INSERT INTO events(owner_id, name, description, date, location) VALUES ($1, $2, $3, $4, $5) RETURNING id"
+
+	return nil
 }
